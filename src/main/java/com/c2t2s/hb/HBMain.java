7afceff10823f7ae90a85da0e8e49a675ad6a3be
100PercentHippo <100PercentHippo@gmail.com>
2021-03-07 20:58:32 -0500

    Adds command to registered commands

--- src/main/java/com/c2t2s/hb/HBMain.java
+++ src/main/java/com/c2t2s/hb/HBMain.java
@@ -24,6 +24,8 @@ public class HBMain {
         commands.put("r", HBMain::handleRoll);
         commands.put("version", HBMain::handleVersion);
         commands.put("claim", HBMain::handleClaim);
+        commands.put("balance", HBMain::handleBalance);
+        commands.put("give", HBMain::handleGive);
         final DiscordClient client = new DiscordClientBuilder(args[0]).build();
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .subscribe(HBMain::HandleMessage);
