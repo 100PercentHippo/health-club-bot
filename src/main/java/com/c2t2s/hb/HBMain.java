facd6aaea017f911ce0757a3cc9df222aa8c652b
100PercentHippo <100PercentHippo@gmail.com>
2021-03-08 23:52:57 -0500

    Another attempt

--- src/main/java/com/c2t2s/hb/HBMain.java
+++ src/main/java/com/c2t2s/hb/HBMain.java
@@ -233,10 +233,14 @@ public class HBMain {
     		final String line2 = response.substring(index, index = response.indexOf('\n', index + 1));
     		final String line3 = response.substring(index);
         	event.getChannel().sendMessage(line1).thenAccept(message -> {
-        		message.edit(line1 + line2.substring(0, int index1 = (line2.indexOf("::") + 1))).thenAccept(v -> {
-        			message.edit(line1 + line2.substring(0, int index2 = (line2.indexOf("::", index1 + 1)))).thenAccept(v2 -> {
-            			message.edit(line1 + line2.substring(0, int index3 = (line2.indexOf("::", index2 + 1)))).thenAccept(v3 -> {
-                			message.edit(line1 + line2.substring(0, int index4 = (line2.indexOf("::", index3 + 1)))).thenAccept(v4 -> {
+        		int index1;
+        		message.edit(line1 + line2.substring(0, index1 = (line2.indexOf("::") + 1))).thenAccept(v -> {
+        			int index2;
+        			message.edit(line1 + line2.substring(0, index2 = (line2.indexOf("::", index1 + 1)))).thenAccept(v2 -> {
+        				int index3;
+            			message.edit(line1 + line2.substring(0, index3 = (line2.indexOf("::", index2 + 1)))).thenAccept(v3 -> {
+            				int index4;
+                			message.edit(line1 + line2.substring(0, index4 = (line2.indexOf("::", index3 + 1)))).thenAccept(v4 -> {
                 				message.edit(line1 + line2 + line3);
                 			});
             			});
