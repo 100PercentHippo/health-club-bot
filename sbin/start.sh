b336e65f390366e1e55635debbc97f0c9bd28229
100PercentHippo <100PercentHippo@gmail.com>
2023-01-01 18:28:25 -0500

    Configure DB variables in start script

--- sbin/start.sh
+++ sbin/start.sh
@@ -6,5 +6,13 @@ export API_KEY=$(curl "https://secretmanager.googleapis.com/v1/projects/norse-se
     --header "authorization: Bearer $(gcloud auth print-access-token)" \
     --header "content-type: application/json" | jq -r .payload.data | base64 -d)
 
+# Configure JDBC environment variables
+export JDBC_DATABASE_URL="jdbc:postgresql://35.233.156.255:5432/postgres"
+export JDBC_USERNAME=postgres
+export JDBC_PASSWORD=$(curl "https://secretmanager.googleapis.com/v1/projects/norse-sequence-373221/secrets/DB_PASSWORD/versions/1:access" \
+    --request "GET" \
+    --header "authorization: Bearer $(gcloud auth print-access-token)" \
+    --header "content-type: application/json" | jq -r .payload.data | base64 -d)
+
 # Run the server
 java -cp target/classes/:target/dependency/* com.c2t2s.hb.HBMain $API_KEY
