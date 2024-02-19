f66a0935310f4be1e49ff09f7754c6f17dc5dbc3
100PercentHippo <100PercentHippo@gmail.com>
2024-02-19 14:03:17 -0500

    Create new local start script

---
+++ sbin/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check for running server
+if screen -ls | grep -q "casino"; then
+  echo "Server already running"
+  return -1 2>/dev/null || exit -1;
+fi
+
+# Ensure API Token is set
+if [ -z "$BOT_TOKEN" ]; then
+  echo "No API Token set. Should be set via the environment variable BOT_TOKEN"
+  return -1 2>/dev/null || exit -1;
+fi
+
+# Run the server
+datetime=$(date +"%Y%m%d-%H%M%S")
+screen -dmS casino bash -c "java -cp target/classes/:target/dependency/* com.c2t2s.hb.HBMain $BOT_TOKEN > \"logs/log-$datetime.log\""
+ln -s "logs/log-$datetime.log" logs/log-current.log
+
+echo "Server started successfully"
