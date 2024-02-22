dc369969cf8d2143d310e333161e38faf29f64f8
100percenthippo <100percenthippo@gmail.com>
2024-02-21 19:58:12 -0500

    Add init support to start script and fix log-current

--- sbin/start.sh
+++ sbin/start.sh
@@ -12,13 +12,32 @@ if [ -z "$BOT_TOKEN" ]; then
   return -1 2>/dev/null || exit -1;
 fi
 
+# Check if we should initialize commands
+if [ "$#" -gt 0 ]
+then
+  if [ "$1" == "init" ]
+  then
+    init=true
+  else
+    init=
+  fi
+fi
+
 # Run the server
 datetime=$(date +"%Y%m%d-%H%M%S")
-screen -dmS casino bash -c "java -cp target/classes/:target/dependency/* com.c2t2s.hb.HBMain $BOT_TOKEN > logs/log-$datetime.log"
+if [ -n "$init" ]
+then
+  screen -dmS casino bash -c "java -cp target/classes/:target/dependency/* com.c2t2s.hb.HBMain $BOT_TOKEN init > logs/log-$datetime.log 2>&1"
+else
+  screen -dmS casino bash -c "java -cp target/classes/:target/dependency/* com.c2t2s.hb.HBMain $BOT_TOKEN > logs/log-$datetime.log 2>&1"
+fi
+
 if [ -e logs/log-current.log ]
 then
   rm logs/log-current.log
 fi
-ln -s "logs/log-$datetime.log" logs/log-current.log
+cd logs
+ln -s log-$datetime.log log-current.log
+cd ..
 
 echo "Server started successfully"
